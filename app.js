6e35eef64b6e9ec3049088732ab2521cc3820337
Guruteja Kanderi <105558277+Kanderi-guruteja@users.noreply.github.com>
2023-11-28 16:32:14 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,39 +1,45 @@
+// Define the functio to get geocode API
 $(document).ready(function () {
  function fetchLocationCoordinates(location) {
-  const geocodeApiUrl = `https://geocode.maps.co/search?q=${encodeURIComponent(location)}`;
-
+  const geocodeApiUrl = `https://geocode.maps.co/search?q=${encodeURIComponent(location)}`; // Geocode API URL to search the location
+//initiating the ajax request to geocode API
   $.ajax({
    url: geocodeApiUrl,
    method: "GET",
    success: function (geocodeData) {
     const results = geocodeData;
-    console.log(results);
+    console.log(results);//Get the response from function
 
+    //To check if the searched location was found or not fron the user input
     if (results && results.length > 0) {
      const firstResult = results[0];
      const latitude = firstResult.lat;
      const longitude = firstResult.lon;
+     //Date Selector to select the date to get the sunrise and sunset data
      const selectedDate = $("#dateSelector").val();
+     //get the data using the latitude and longitude from geocode api
      fetchSunriseSunsetData(latitude, longitude, selectedDate);
     } else {
+     //Exception handling
      displayError("Location not found.");
     }
    },
    error: function (error) {
-    displayError(`Geocode API Error: ${error.responseJSON.status}`);
+    displayError(`Geocode API Error while fetching location: ${error.responseJSON.status}`);
    },
   });
  }
 
  function fetchSunriseSunsetData(latitude, longitude, date) {
   const sunriseSunsetApiUrl = `https://api.sunrisesunset.io/json?lat=${latitude}&lng=${longitude}&formatted=0&date=${date}`;
-
+//Send the ajax request to sunrisesunsetAPI
   $.ajax({
    url: sunriseSunsetApiUrl,
    method: "GET",
    success: function (data) {
     updateDashboard(data.results);
    },
+   //Error handling for sunrisesunset API
    error: function (error) {
     displayError(`Sunrise Sunset API Error: ${error.responseJSON.status}`);
    },
@@ -69,6 +75,7 @@ $(document).ready(function () {
     fetchSunriseSunsetData(latitude, longitude, selectedDate);
    },
    function (error) {
+    //Eception for Geolocation
     displayError(`Geolocation Error: ${error.message}`);
    }
   );
